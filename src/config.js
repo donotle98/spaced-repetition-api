76a0eaf542d45d511b2d56eaefff3e91bb14a96b
Donovan <donovanle18@gmail.com>
2020-12-18 13:11:06 -0600

    config changes

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || "development",
     DATABASE_URL:
-        process.env.DB_URL || "postgresql://dono@localhost/spacedrepetition",
+        process.env.DATABASE_URL ||
+        "postgresql://dono@localhost/spacedrepetition",
     JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
     JWT_EXPIRY: process.env.JWT_EXPIRY || "3h",
 };
